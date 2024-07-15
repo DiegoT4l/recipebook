8760b797da2aa76f775efffe9d0dc87ee37bad8b
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-07-15 15:09:25 -0600

    chore: Update Footer component with new styles and content

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,36 @@
+import styles from './Footer.module.css';
+
 function Footer() {
     return (
-        <footer className="bg-dark text-white text-center p-3">
-            <p>© 2021 - Todos los derechos reservados</p>
-        </footer>
+        
+
+<footer className={styles['footer']}>
+    <div className={styles['footer-container']}>
+        <div className={styles['footer-content']}>
+            <a href="#" className={styles['footer__head--link']}>
+                <span className={styles['footer__head--title']}>Recipebook.io</span>
+            </a>
+            <ul className={styles['footer__navbar']}>
+                <li>
+                    <a href="#" className={styles['footer__navbar--link']}>About</a>
+                </li>
+                <li>
+                    <a href="#" className={styles['footer__navbar--link']}>Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className={styles['footer__navbar--link']}>Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className={styles['footer__navbar--link']}>Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className={styles['footer__divisor']} />
+        <span className={styles['footer__copyrigth']}>© 2023 <a href="#" class="hover:underline">Recipebook.io™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
     );
 }
 

13566c7e76fe4d14976a3deff13d5100f095a82c
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-07-15 13:40:53 -0600

    chore: Update Header module styles and import video from assets

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,12 +1,15 @@
+import styles from './Header.module.css';
+import heroVideo from '../../assets/videos/hero_video.mp4';
+
 function Header() {
 
     return (
-        <header className="header">
+        <header className={styles.header}>
             <video autoPlay loop muted>
-                <source src="./assets/videos/cover1.mp4" />
+                <source src={heroVideo} />
             </video>
-            <h1 className="header__title glass-effect">&nbsp;&nbsp;&nbsp;&nbsp;RECIPEBOOK.IO&nbsp;&nbsp;&nbsp;&nbsp;</h1>
-            <h2 className="header__slogan">Simple Ingredients, Extraordinary Flavors.</h2>
+            <h1 className={styles["header__title"]}>&nbsp;&nbsp;&nbsp;&nbsp;RECIPEBOOK.IO&nbsp;&nbsp;&nbsp;&nbsp;</h1>
+            <h2 className={styles["header__slogan"]}>Simple Ingredients, Extraordinary Flavors.</h2>
         </header>
     );
 }
